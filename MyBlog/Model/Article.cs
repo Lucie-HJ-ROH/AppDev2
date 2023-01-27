54a06f24423ea21935771499238683ecb1a883bb
Lucie-HJ-ROH <lucie.hj.roh@gmail.com>
2023-01-27 10:15:25 -0500

    add model

---
+++ MyBlog/Model/Article.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace MyBlog.Model;
+
+public class Article
+{
+    public int Id { get; set; }
+    public IdentityUser Author { get; set; }
+    public string Title { get; set; }
+    public string Body { get; set; }
+    public DateTime Created { get; set; }
+}
